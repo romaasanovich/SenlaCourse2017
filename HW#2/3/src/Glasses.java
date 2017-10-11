d26bf11259f4bce5f57421c8f549357be5b924a8
romaasanovich <asanovich.roma@gmail.com>
2017-10-11 08:39:41 +0300

    Added assignment of constituent parts

--- HW#2/3/src/Glasses.java
+++ HW#2/3/src/Glasses.java
@@ -1,14 +1,21 @@
 
 public class Glasses implements IProduct {
-	public void installFirstPart(IProductPart body) {
+	private Body body;
+	private Bow bow;
+	private Lens lens;
+	
+	public void installFirstPart(IProductPart part) {
+		body=(Body)part;
 		System.out.println("Install body");
 	}
 	
-	public void installSecondPart(IProductPart bow) {
+	public void installSecondPart(IProductPart part) {
+		bow=(Bow) part;
 		System.out.println("Install bows");
 	}
 	
-	public void installThirdPart(IProductPart lens) {
+	public void installThirdPart(IProductPart part) {
+		lens=(Lens) part;
 		System.out.println("Install lenses");
 	}
 }
