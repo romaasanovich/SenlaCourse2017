7f8fc14a192788eedb9fe480b41e67b0c933cada
romaasanovich <asanovich.roma@gmail.com>
2018-02-25 21:52:13 +0300

    changes

--- Programm/sever/src/com/senla/autoservice/utills/Convert.java
+++ Programm/sever/src/com/senla/autoservice/utills/Convert.java
@@ -58,49 +58,6 @@ public class Convert {
 			return 0;
 	}
 
-	public final static Place fromStrToPlace(String line) {
-		String[] temp = line.split(",");
-		int id = Integer.parseInt(temp[0]);
-		boolean isBusy = fromIntToBooleanSQL(temp[2]);
-		Place place = new Place(id, temp[1], isBusy);
-		return place;
-	}
-
-	public final static Work fromStrToWork(String line) {
-		String[] temp = line.split(",");
-		int id = Integer.parseInt(temp[0]);
-		double price = Double.parseDouble(temp[2]);
-		int idMaster = Integer.parseInt(temp[3]);
-		Master master = new Master(idMaster, null, false);
-		Work work = new Work(id, temp[1], price, master);
-		return work;
-	}
-
-	public final static Master fromStrToMaster(String line) {
-		String[] temp = line.split(",");
-		int id = Integer.parseInt(temp[0]);
-		boolean isWork = fromIntToBooleanSQL(temp[2]);
-		Master master = new Master(id, temp[1], isWork);
-		return master;
-	}
-	
-	public final static Order fromStrToOrder(String line) {
-		String[] temp = line.split(",");
-		int id = Integer.parseInt(temp[0]);
-		int idService = Integer.parseInt(temp[1]);
-		int idMaster= Integer.parseInt(temp[2]);
-		int idPlace = Integer.parseInt(temp[3]);
-		StatusOrder st = fromStrToStatus(temp[4]) ;
-		Date ordDate = fromStrToDate(temp[5]);
-		Date startDate = fromStrToDate(temp[6]);
-		Date endDate = fromStrToDate(temp[7]);
-		Work work = new Work(idService,null,0,null);
-		Master master = new Master(idMaster,null,false);
-		Place place = new Place (idPlace,null,false);
-		Order order  = new Order(id,master,work,place,st,ordDate,startDate,endDate);
-		return order;
-	}
-
 	public final static Date fromStrToDate(String line) {
 		String[] tempDate = line.split("-");
 		GregorianCalendar grCal = new GregorianCalendar(Integer.parseInt(tempDate[0]), Integer.parseInt(tempDate[1]),
